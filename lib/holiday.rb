78df83f5894152a83364030bab8c27d38ff1e404
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 22:38:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.collect do |key, value|
+  holiday_hash.each do |key, value|
     new_key = key.capitalize
     puts "#{new_key}:"
     value.each do |season, item|
