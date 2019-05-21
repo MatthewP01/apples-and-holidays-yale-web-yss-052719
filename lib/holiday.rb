376a0c81d97515f60c400aa69236e511fb5da0f9
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 22:45:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,5 +74,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  holiday_hash.collect do |key, value|
+    
 end
