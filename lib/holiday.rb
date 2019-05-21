e5a2395e225f46d3fa75dfc8bea68be74de16da9
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 22:34:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
       season_ = season.to_s.split("_").map{|cap| cap.capitalize!}
       season_done = season_.join(" ")
       item_done = item.join(", ")
-      puts "#{season_done}: #{item_done}"
+      puts "   #{season_done}: #{item_done}"
     end
     puts "#{new_key}: #{value}"
   end
