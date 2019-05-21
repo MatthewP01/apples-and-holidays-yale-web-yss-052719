d3dd35d7a559dcd687b9dde169ce7813f7c54257
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 22:38:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
 #     new_key = key.capitalize
 #     puts "#{new_key}:"
 #     value.each do |season, item|
-#       season_ = season.to_s.split("_").map{|cap| cap.capitalize!}
+#       season_ = season.to_s.split("_").map{|cap| cap.capitalize!}.join(" ")
 #       season_done = season_.join(" ")
 #       item_done = item.join(", ")
 #       puts "  #{season_done}: #{item_done}"
@@ -71,13 +71,13 @@ def all_supplies_in_holidays(holiday_hash)
 #     puts "#{new_key}: #{value}"
 #   end
 # end
-  holiday_hash.each do |season, holiday|
-      puts "#{season.capitalize}:"
-      holiday.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
-    end
-  end
-  end
+  # holiday_hash.each do |season, holiday|
+  #     puts "#{season.capitalize}:"
+  #     holiday.each do |holiday, supplies|
+  #     puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  #   end
+  # end
+  # end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
